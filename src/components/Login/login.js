c352044b85ade388dd964852691e88950cf83f74
Osorabrian <osorabrian@gmail.com>
2023-03-26 11:10:11 +0300

    format log in page

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './login.css'
 
 export default function Login(){
     return(
-        <>
-            <form>
+        <div id='login-page'>
+            <form id='login-form'>
+
+                <h2 style={{textAlign:'center'}}>Log in</h2>
+
                 <div className='row mb-3'>
                     <label className='form-label'>User name:</label>
-                    <input placeholder='Enter User name'className='form-control' type='text' />
+                    <input placeholder='Enter User Name'className='form-control' type='text' />
                 </div>
                 
                 <div className='row mb-3'>
                     <label className='form-label'>Password</label>
-                    <input placeholder='password' className='form-control' type = 'password'/>
+                    <input placeholder='Enter Password' className='form-control' type = 'password'/>
                 </div>
+
+                <p className='form-text'>Don't have an account ? <NavLink to='/signup'> Create an acount.</NavLink></p>
                 
+                <button className='btn btn-primary'>Log in</button>
             </form>
-        </>
+        </div>
     )
 }
