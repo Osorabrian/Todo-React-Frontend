1bf73647d138a10554d1e8c9af4811b51cd0a5ce
Osorabrian <osorabrian@gmail.com>
2023-03-26 12:12:10 +0300

    format log in and sign up form

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -10,18 +10,21 @@ export default function Login(){
                 <h2 style={{textAlign:'center'}} className='mb-4'>Log in</h2>
 
                 <div className='row mb-4'>
-                    <label className='form-label'>User name:</label>
-                    <input placeholder='Enter User Name'className='form-control' type='text' />
+                    <label className='form-label'>User Name:</label>
+                    <input placeholder='Enter User Name'className='form-control' type='text' required/>
                 </div>
                 
                 <div className='row mb-4'>
                     <label className='form-label'>Password</label>
-                    <input placeholder='Enter Password' className='form-control' type = 'password'/>
+                    <input placeholder='Enter Password' className='form-control' type = 'password' required/>
                 </div>
 
                 <p className='form-text'>Don't have an account ? <NavLink to='/signup'> Create an acount.</NavLink></p>
                 
-                <button className='btn btn-primary'>Log in</button>
+                <div style={{textAlign: 'center'}}>
+                    <button className='btn btn-primary' style={{width: '100px'}}>Log in</button>
+                </div>
+
             </form>
         </div>
     )
--- src/components/Signup/signupform.js
+++ src/components/Signup/signupform.js
@@ -6,33 +6,43 @@ export default function SignUpForm(){
     return(
         <form id='signup-form'>
 
-            <h2>Sign Up</h2>
+            <h2 className="mb-2" style={{textAlign: 'center'}}>Sign Up</h2>
 
             <div className="row mb-3">
-                <label className="form-label">Username:</label>
+
+                <label className="form-label">User Name:</label>
                 <input className='form-control' placeholder='Enter User name' type='text' id='signup-email' required/>
+
             </div>
 
             <div className="row mb-3">
+
                 <label className="form-label">Email</label>
                 <input className="form-control" placeholder="Enter Email" type={'email'} id='signup-email' required/>
+
             </div>
 
             <div className="row mb-3">
-                <label className="form-label">Password</label>
-                <input className="form-control" placeholder="Enter Password" type={'password'} id='password' required/>
-            </div>
 
-            <div className="row mb-3">
-                <label className="form-label">Password</label>
-                <input className="form-control" placeholder="Repeat Password" type={'password'} id='repeat-password' required/>
+                <div className="col-6">
+                    <label className="form-label">Password</label>
+                    <input className="form-control" placeholder="Enter Password" type={'password'} id='password' required/>
+                </div>
+               
+               <div className="col-6">
+                    <label className="form-label">Password</label>
+                    <input className="form-control" placeholder="Repeat Password" type={'password'} id='repeat-password' required/>
+               </div>
+    
             </div>
 
             <div className="row mb-3">
                 <NavLink className={'form-text'} to='/'>Already have an account ? </NavLink>
             </div>
 
-            <button className="btn btn-primary">Create account</button>
+            <div style={{textAlign: 'center'}}>
+            <button className="btn btn-primary" style={{width: '190px'}}>Create account</button>
+            </div>
 
         </form>
     )
