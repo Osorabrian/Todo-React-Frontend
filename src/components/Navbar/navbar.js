a5f330b6464a02427ae5e90a15bf99634ee0d79d
Osorabrian <osorabrian@gmail.com>
2023-03-26 10:54:36 +0300

    add bootstrap CDN in index.html"

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -5,11 +5,11 @@ export default function Navbar(){
     return(
         <div className='navbar'>
             <NavLink to='/' className={'navbar-brand'}>Todo</NavLink>
-            <ul>
-                <NavLink to='/' className={'nav-item'}>Log in</NavLink>
-                <NavLink to="/signup" className={'nav-item mb-3'}>Sign up</NavLink>
-                <NavLink to='/logout' className={'nav-item'}>Log out</NavLink>
-                <NavLink to='/home' className={'nav-item'}>Home</NavLink>
+            <ul className='me-auto'>
+                <NavLink to='/' className={'nav-item me-3'}>Log in</NavLink>
+                <NavLink to="/signup" className={'nav-item me-3'}>Sign up</NavLink>
+                <NavLink to='/logout' className={'nav-item me-3'}>Log out</NavLink>
+                <NavLink to='/home' className={'nav-item me-3'}>Home</NavLink>
             </ul>
         </div>
     )
