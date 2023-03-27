4709492b94456b2abaf1309e3dfa8839b29de360
Osorabrian <osorabrian@gmail.com>
2023-03-27 05:22:27 +0300

    add styling to navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './navbar.css'
 
 export default function Navbar(){
     return(
-        <div className='navbar'>
+        <div className='navbar' id='navbar'>
+
             <NavLink to='/' className={'navbar-brand'}>Todo</NavLink>
-            <ul className='me-auto'>
-                <NavLink to='/todos' className={'nav-item me-3'}>Todos</NavLink>
-                <NavLink to='/create-todos' className={'nav-item me-3'}>Create Todos</NavLink>
-                <NavLink to='/' className={'nav-item me-3'}>Log out</NavLink>
-                <NavLink to='/' className={'nav-item me-3'}>Log in</NavLink>
-                <NavLink to="/signup" className={'nav-item me-3'}>Sign up</NavLink>
+
+            <ul className='nav justify-content-center'>
+                <NavLink to='/todos' className={'nav-link me-3'}>Todos</NavLink>
+                <NavLink to='/create-todos' className={'nav-link me-3'}>Create Todos</NavLink>
             </ul>
+
+            <ul className='nav justify-content-right'>
+                <NavLink to='/' className={'nav-link me-3'}>Log out</NavLink>
+                <NavLink to='/' className={'nav-link me-3'}>Log in</NavLink>
+                <NavLink to="/signup" className={'nav-link me-3'}>Sign up</NavLink>
+            </ul>
+            
         </div>
     )
 }
